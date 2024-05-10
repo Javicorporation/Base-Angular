43c214588b35cfd23e28a7a771551f89e50fd3f4
Javicorporation <krchipullajavier@gmail.com>
2024-05-10 14:37:33 -0500

    implementacion del enrutamiento y modificacion en app-component.html para mostrar los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +20,7 @@ import { HomeComponent } from './pages/home/home.component';
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+  
   ],
   bootstrap: [AppComponent]
 })
