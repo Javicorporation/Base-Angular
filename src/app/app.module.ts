8c0d5ec7f70e1e6f3614430431efacfb2d4f86f7
Javicorporation <krchipullajavier@gmail.com>
2024-05-13 18:40:20 -0500

    modificamos app.module.ts y creamos pages.modules.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { HomeComponent } from './pages/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    ContactComponent,
-    HomeComponent,
+    //AboutComponent,
+    //ContactComponent,
+    //HomeComponent,
     MenuComponent
   ],
   imports: [
