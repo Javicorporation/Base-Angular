27027630289baabf70c82c805682d376d99b140d
Javicorporation <krchipullajavier@gmail.com>
2024-05-13 19:59:07 -0500

    modificamos menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
+
+  rutas = [
+    {
+    name: 'Home',
+    path: '/home'
+    },
+    {
+    name: 'About',
+    path: '/about'
+    },
+    {
+    name: 'Contact',
+    path: '/contact'
+    },
+    {
+    name: 'Posts',
+    path: '/posts'
+    },
+    ];
+    constructor() { }
+    ngOnInit(): void {
+    }
 
 }
