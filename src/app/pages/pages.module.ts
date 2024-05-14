eb5525db0ae3f3063f87df1f597bc8558061c13e
Javicorporation <krchipullajavier@gmail.com>
2024-05-13 19:02:11 -0500

    creamos pages.ts y modificamos pages.module.ts

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  exports: [
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+
+  ],
   imports: [
     CommonModule
   ]
